a804a0c15a9aec574edadff9ee277b7318207044
hwnim5324 <hwnim5324@naver.com>
2022-10-24 22:52:34 +0900

    dao settings

--- src/main/java/com/niceCode/community/DAO/UsersDAO.java
+++ src/main/java/com/niceCode/community/DAO/UsersDAO.java
@@ -1,5 +1,23 @@
 package com.niceCode.community.DAO;
 
+import com.niceCode.community.DTO.UsersDTO;
+import com.niceCode.community.mapper.UsersDTOMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
 public class UsersDAO {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<UsersDTO> findByUserCode(int userCode){
+        RowMapper<UsersDTO> mapper = new UsersDTOMapper();
+        String query = "SELECT * FROM users WHERE userCode = ?";
+
+        return jdbcTemplate.query(query, mapper, userCode);
+    }
 }
+
